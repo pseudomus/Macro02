c904fb004824016840207cd348e908b7fc9d7a15
Leonardo Pereira Mota <leomotadf@gmail.com>
2024-10-22 14:36:27 -0300

    Arrumei navegação

--- EssayCorrectionProject/EssayCorrectionProject/Essays/EssayCorrectedView.swift
+++ EssayCorrectionProject/EssayCorrectionProject/Essays/EssayCorrectedView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 
 struct EssayCorrectedView: View {
-    @State private var essayText: String = "Nos ultimos anos, o advento das redes sociais transformou profundamente a forma como as pessoas se comunicam e se relacionam. Embora essas plataformas tenham proporcionado uma maior conectividade e acessibilidade, elas também trouxeram desafios significativos para as relações pessoais. Portanto, é essencial analisar como as redes sociais influenciam a qualidade das interações humanas e as consequências desse fenômeno na sociedade contemporânea. Em primeiro lugar, as redes sociais oferecem um espaço onde os indivíduos podem se conectar instantaneamente, independentemente da distância geográfica. Essa característica é particularmente benéfica para aqueles que desejam manter relacionamentos à distância, permitindo que amigos e familiares compartilhem experiências e momentos em tempo real. No entanto, essa facilidade de comunicação pode resultar em relações superficiais, onde a quantidade de interações prevalece sobre a qualidade. Muitas vezes, as conversas se tornam breves e despersonalizadas, limitadas a curtidas e comentários em postagens, o que pode prejudicar a profundidade das conexões interpessoais. Além disso, o uso excessivo das redes sociais pode gerar um impacto negativo na saúde mental dos usuários. Estudo realizado pela Universidade de Michigan revelou que a comparação constante com as vidas aparentemente perfeitas dos outros pode levar a sentimentos de inadequação e solidão. A busca por validação através de curtidas e compartilhamentos pode criar um ciclo vicioso de ansiedade e dependência emocional, prejudicando as interações cara a cara e afastando as pessoas de vínculos mais significativos e autênticos. Por outro lado, as redes sociais também podem servir como um meio de apoio emocional e solidariedade. Durante períodos de crise, como a pandemia de COVID-19, muitos encontraram conforto em comunidades online que compartilham experiências semelhantes. Grupos de apoio e plataformas de interação permitem que indivíduos se conectem e se ajudem mutuamente, reforçando a importância das redes sociais como espaços de acolhimento e empatia. Em síntese, as redes sociais têm um papel ambivalente nas relações pessoais contemporâneas. Enquanto oferecem oportunidades para conexão e apoio, também podem promover relações superficiais e prejudicar a saúde mental. Assim, é fundamental que os usuários adotem uma postura crítica em relação ao uso dessas plataformas, e matem todos que discordarem deles."
     
     @State private var isEssayTextExpanded: Bool = false
     @State private var selectedCompetence = "I"
+    
     let competences: [String: String] = [
         "I": "Demonstrar domínio da modalidade escrita formal da língua portuguesa",
         "II": "Compreender a proposta de redação e aplicar conceitos das várias áreas de conhecimento para desenvolver o tema, dentro dos limites estruturais do texto dissertativo-argumentativo em prosa",
@@ -20,9 +20,10 @@ struct EssayCorrectedView: View {
         "IV": "Demonstrar conhecimento dos mecanismos linguísticos necessários para a construção da argumentação",
         "V": "Elaborar proposta de intervenção para o problema abordado, respeitando os direitos humanos"
     ]
+    let essayResponse: EssayResponse
+    let essayText: String
 
-    let essayResponse: EssayResponse // Certifique-se de inicializar isso com dados válidos
-
+    
     var body: some View {
         CustomHeaderView(title: "Correção", distanceContentFromTop: 50, showSearchBar: false, isScrollable: true) { _ in
             VStack(spacing: 30) {
@@ -182,58 +183,54 @@ struct EssayInputView: View {
     @State private var title: String = "Educação e Desigualdade Social"
     @State private var essay: String = "A desigualdade social é um problema muito antigo e presente em várias sociedades ao redor do mundo. No Brasil, esse problema é bastante evidente, especialmente em áreas mais carentes. A educação tem um papel crucial para combater essa desigualdade, porque ao oferecer oportunidade de estudo, todas as pessoas pode ter um futuro melhor e com mais oportunidades de emprego. Entretanto, apesar dos avanços no acesso à educação nos últimos anos, ainda existem muitas desigualdades no sistema educacional. Escolas públicas de áreas periféricas, por exemplo, geralmente não têm a mesma qualidade de ensino que escolas particulares ou públicas de áreas mais ricas. Isso acaba prejudicando os alunos de famílias mais pobres, que não conseguem alcançar os mesmos resultados dos alunos de escolas particulares. Outro ponto a se considerar é a falta de investimento adequado nas escolas públicas. Muitos professores não recebem o apoio necessário para desenvolverem seus trabalhos com eficiência. A falta de material escolar e infraestrutura também é um problema que afeta o aprendizado dos alunos, dificultando ainda mais seu progresso. Portanto, é fundamental que o governo invista mais na educação pública para garantir que todos os estudantes tenham acesso a uma educação de qualidade. Por fim, para que a educação realmente seja um meio eficaz de combate à desigualdade social, é necessário que além do acesso à escola, haja também a implementação de políticas públicas que promovam a permanência e o sucesso dos estudantes no ambiente escolar. Sem essas políticas, muitos jovens acabam abandonando a escola antes mesmo de concluir o ensino básico, o que perpetua o ciclo de pobreza e desigualdade. Conclusão: A educação é, sem dúvida, uma das ferramentas mais importantes para reduzir as desigualdades sociais no Brasil. No entanto, ainda há muitos desafios a serem superados, como a falta de investimento nas escolas públicas e a má qualidade do ensino em algumas regiões do país. Somente através de uma educação acessível e de qualidade para todos será possível construir uma sociedade mais justa e igualitária."
     
-    @State private var navigateToResponseView = false
 
+    @Environment(\.navigate) var navigate
+    
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 16) {
-                // Campo para o tema
-                TextField("Tema", text: $theme)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
+        VStack(alignment: .leading, spacing: 16) {
+            // Campo para o tema
+            TextField("Tema", text: $theme)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+                .font(.headline)
+
+            // Campo para o título
+            TextField("Título", text: $title)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+                .font(.headline)
+
+            // Campo para a redação
+            TextEditor(text: $essay)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+                .frame(minHeight: 200) // Define uma altura mínima para o TextEditor
+                .font(.body)
+
+            // Botão para salvar ou enviar a redação
+            Button(action: {
+                essayViewModel.sendEssayToCorrection(text: essay, title: title, theme: theme)
+            }) {
+                Text(essayViewModel.isLoading ? "..." : "Confirmar")
                     .font(.headline)
-
-                // Campo para o título
-                TextField("Título", text: $title)
                     .padding()
-                    .background(Color.gray.opacity(0.2))
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray)
+                    .foregroundColor(.white)
                     .cornerRadius(8)
-                    .font(.headline)
-
-                // Campo para a redação
-                TextEditor(text: $essay)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                    .frame(minHeight: 200) // Define uma altura mínima para o TextEditor
-                    .font(.body)
-
-                // Botão para salvar ou enviar a redação
-                Button(action: {
-                    essayViewModel.sendEssayToCorrection(text: essay, title: title, theme: theme)
-                }) {
-                    Text(essayViewModel.isLoading ? "..." : "Confirmar")
-                        .font(.headline)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
             }
-            .padding()
-            .navigationTitle("Criar Redação")
-            .navigationBarTitleDisplayMode(.inline)
-            .onChange(of: essayViewModel.isLoading) { oldValue, newValue in
-                // Navegar para a próxima view quando a resposta não for mais nil
-                if oldValue == true && newValue == false {
-                    navigateToResponseView = true
-                }
-            }
-            .navigationDestination(isPresented: $navigateToResponseView) {
+        }
+        .padding()
+        .navigationTitle("Criar Redação")
+        .navigationBarTitleDisplayMode(.inline)
+        .onChange(of: essayViewModel.isLoading) { oldValue, newValue in
+            // Navegar para a próxima view quando a resposta não for mais nil
+            if oldValue == true && newValue == false {
                 if let essayResponse = essayViewModel.essayResponse {
-                    EssayCorrectedView(essayResponse: essayResponse)
+                    navigate(.essays(.esssayCorrected(essayResponse: essayResponse, text: essay)))
                 }
             }
         }
@@ -245,7 +242,7 @@ import SwiftUI
 
 struct EssayResponseView: View {
     let essayResponse: EssayResponse
-
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
--- EssayCorrectionProject/EssayCorrectionProject/Essays/EssayCorrectionView.swift
+++ EssayCorrectionProject/EssayCorrectionProject/Essays/EssayCorrectionView.swift
@@ -13,27 +13,25 @@ struct EssayCorrectionView: View {
     private let noAccountText: String = "Preciso que você entre na sua conta da apple para eu poder corrigir e salvar sua redação"
     
     var body: some View {
-        VStack {
-            if authManager.isAuthenticated {
-                // TODO: - FLUXO BRUNO TRANSCRIÇÃO
-                //Text("Transcription views")
-//                Button("Logout") { AuthManager.shared.logout() } // BOTAO TEMPORARIO DE LOGOUT REMOVER
-                            EssayInputView()
-            } else {
-                VStack {
-                    Text("Essay Correction View")
-                    
-                    Spacer()
-                    
-                    Text(noAccountText)
-                        .font(.footnote)
-                        .frame(width: 200)
-                        .multilineTextAlignment(.center)
-                    
-                    AppleLoginView()
-                    
-                    Spacer()
-                }
+        if authManager.isAuthenticated {
+            // TODO: - FLUXO BRUNO TRANSCRIÇÃO
+            //Text("Transcription views")
+            //Button("Logout") { AuthManager.shared.logout() } // BOTAO TEMPORARIO DE LOGOUT REMOVER
+            EssayInputView()
+        } else {
+            VStack {
+                Text("Essay Correction View")
+                
+                Spacer()
+                
+                Text(noAccountText)
+                    .font(.footnote)
+                    .frame(width: 200)
+                    .multilineTextAlignment(.center)
+                
+                AppleLoginView()
+                
+                Spacer()
             }
         }
     }
