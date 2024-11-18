972b9e5a120dd8578da770de447a94d5df73852b
Bruno Teodoro <brunoamteodoro@gmail.com>
2024-11-18 15:04:22 -0300

    Implemented Ads

--- EssayCorrectionProject/EssayCorrectionProject/Google-Admob/RewardedAd.swift
+++ EssayCorrectionProject/EssayCorrectionProject/Google-Admob/RewardedAd.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import GoogleMobileAds
+import SwiftUI
 
 class RewardedAd: NSObject, GADFullScreenContentDelegate, ObservableObject {
-    @Published var rewardedAd: GADRewardedAd?
-    //MARK: Essa é a função chamada quando o usuário assiste o vídeo até o final.
+    @Published var rewardedAd: GADRewardedAd? = nil
+    @Published var isAdReady: Bool = false
+
     var rewardEarnedCallback: (() -> Void)?
 
     override init() {
@@ -20,42 +22,66 @@ class RewardedAd: NSObject, GADFullScreenContentDelegate, ObservableObject {
 
     func loadRewardedAd() {
         rewardedAd = nil
-        let testAdUnitID = "ca-app-pub-3940256099942544/5224354917"
+        isAdReady = false
         
+        let testAdUnitID = "ca-app-pub-3940256099942544/5224354917"
         GADRewardedAd.load(withAdUnitID: testAdUnitID, request: GADRequest()) { [weak self] ad, error in
             if let error = error {
                 print("Failed to load rewarded ad with error: \(error.localizedDescription)")
                 return
             }
-            self?.rewardedAd = ad
-            self?.rewardedAd?.fullScreenContentDelegate = self
-            print("Rewarded ad loaded.")
+            
+            DispatchQueue.main.async {
+                self?.rewardedAd = ad
+                self?.rewardedAd?.fullScreenContentDelegate = self
+                self?.isAdReady = true
+                print("Rewarded ad loaded and ready.")
+            }
+        }
+    }
+
+    func displayRewardedAd() {
+        guard isAdReady, let rewardedAd = rewardedAd else {
+            print("Ad not ready for presentation.")
+            return
+        }
+
+        DispatchQueue.main.async {
+            if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+                var topController = rootViewController
+                while let presentedVC = topController.presentedViewController {
+                    topController = presentedVC
+                }
+                
+                rewardedAd.present(fromRootViewController: topController) { [weak self] in
+                    print("User rewarded with 1 reward item.")
+                    self?.rewardEarnedCallback?()
+                    self?.isAdReady = false
+                    self?.loadRewardedAd()
+                }
+            } else {
+                print("Failed to fetch the root view controller.")
+            }
         }
     }
     
-    //MARK: Para mostrar o add com recompensa, basta criar uma instância dessa classe e chamar essa função
-    func displayRewardedAd() {
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let rootVC = windowScene.windows.first?.rootViewController {
-            self.showRewardedAd(from: rootVC)
+    func rewardedAdRetryMechanism() {
+        if isAdReady {
+            displayRewardedAd()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+                self?.rewardedAdRetryMechanism()  
+            }
         }
     }
 
-    func showRewardedAd(from rootViewController: UIViewController) {
-        guard let rewardedAd = rewardedAd else {
-            print("Rewarded ad wasn't ready.")
-            return
-        }
-        
-        rewardedAd.present(fromRootViewController: rootViewController) { [weak self] in
-            print("User rewarded with 1 reward item.")
-            self?.rewardEarnedCallback?()
-        }
-    }
 
     func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
         print("Rewarded ad dismissed.")
+        isAdReady = false
         loadRewardedAd()
     }
 }
+
+
 
